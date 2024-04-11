82be2ffa72e4fc919304eb032577e4f51a16a8d9
Genco Levi <genco.levi@gmail.com>
2024-04-11 14:52:23 +0300

    kinda hacky theme switcher

--- init.lua
+++ init.lua
@@ -100,11 +100,24 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- theme switcher
+-- TODO lualine stuff?
+vim.keymap.set('n', '<leader>tt', function()
+  if vim.g.__bg == 'light' then
+    vim.g.__bg = 'dark'
+    vim.cmd.colorscheme 'molokai'
+    vim.cmd.colorscheme 'molokai'
+  else
+    vim.g.__bg = 'light'
+    vim.cmd.colorscheme 'peachpuff'
+    vim.cmd.colorscheme 'peachpuff'
+  end
+end, { desc = 'Toggle light/dark theme' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
 -- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
